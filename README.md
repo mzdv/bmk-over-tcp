788118ea1b2a266a215ca3ec8727ad64ab960b28
Milos Zivadinovic <mzdv@outlook.com>
2014-12-12 23:32:37 +0100

    Version 0.1 Untested. Errors exist. Will fix them

--- README.md
+++ README.md
@@ -1,25 +1,54 @@
-bmk-over-ip
+bmk-over-tcp
 ===========
-
 Application protocol based on the song "Grga taksista" from Baja Mali Knindza, commonly called as "Vozi me za Surcin"
 or "Vozi me na Pale".
 
-This repository contains both the server and client implementations of the protocl draft.
+DISCLAIMER
+==========
+This was made as a joke, don't take it seriously. It's a good programming exercise to implement your own protocol.
+I learned a ton from it. And now for the fun parts:
+
+Additional info
+---------------
+
+Repository contains both the server and client implementations of the protocol in the form of a message storing server.
 
 BMK protocol is used for transporting data, mostly textual, via TCP using a form of additional encapsulation.
 
+The delimeter is the following sequence: `\/|\|\/`. Every command with multiple parameters must be followed by it for it
+to be registered.
+
+`CRLF` determines the end of the command block (inserted automagically by the client program if they're any good).
+
+Every response from the server is given after a two second lag. This is a commonly requested feature by code reviewers
+(aka my friends; They didn't want to submit a pull request).
+
 The protocol syntax is as follows:
+----------------------------------
 
-VOZI_ME_ZA_SURCIN - announces the existence to the server
+`VOZI_ME_ZA_SURCIN` - announces the existence to the server
 
-PREKO_LEDINA - server response to the announcement
+`PREKO_LEDINA` - server response to the announcement
 
-TAMO_ZIVI|[data] - where [data] is the transmitting data from the client
+`TAMO_ZIVI\/|\|\/[data]` - where [data] is the transmitting data from the client
 
-MOJA_JEDINA - server response to accepted data
+`MOJA_JEDINA` - server response to accepted data
 
-VREME_BRZO_PROLAZI - checking if the server has gotten a timeout
+`VREME_BRZO_PROLAZI` - checking if the server has gotten a timeout
 
-GODINE_ME_STIZU - server response to a negative timeout
+`GODINE_ME_STIZU` - server response to a negative timeout
+
+`DUNI_VJETRE_MALO_PREKO_JETRE` - request for the server to send the contents
+
+`UMRIJECU_OD_BOLA_IZGORJELO_SVE_OD_ALKOHOLA\/|\|\/[messageBag]` - server response followed by the contents of the text
+
+`KUPI_STRIKA_CIPELE_I_DADE_DZEPARAC` - server response to a malformed (or illegal) message
 
 Have fun.
+
+Baja Mali Knindza, all rights reserved (c)
+
+License
+=======
+MIT
+
--- bmkServer.js
+++ bmkServer.js
@@ -3,36 +3,57 @@
  */
 
 var net = require("net");
+
+const LAG = 2000;     //because Baja Mali Knindza needs some time to think
+const DELIMETER = "\\/|\\|\\/"; //if you look closely, it spells out VNV, as in VNV nation
+
 var incomingData = '';
+var messageBag = '';        //structure used for storing data (PoC)
+var reqNumber;      //request number used for logging
 
 net.createServer(function(socket) {
     socket.setEncoding("utf8");
 
-
     socket
         .on("data", function(data) {
             socket.write(data.toString());
+            socket.write('');
             console.log(data.toString());
 
             incomingData += data.toString();
+            reqNumber++;
         })
         .on("end", function() {
-            var message = incomingData.split('|');
+            var message = incomingData.split();
 
             switch(message[0]) {
                 case "VOZI_ME_ZA_SURCIN":
-                    console.log("Aktivirano");
-                    //slanje soketa
+                    setTimeout(function() {
+                        socket.write("PREKO_LEDINA");
+                    }, LAG);
                     break;
+
                 case "TAMO_ZIVI":
-                    console.log("Dolaze podaci");
+                    setTimeout(function() {
+                        messageBag += message[1] + "//" + reqNumber + '\n';
+                        socket.write("MOJA_JEDINA");
+                    }, LAG);
                     break;
+
                 case "VREME_BRZO_PROLAZI":
-                    console.log("Stvarno brzo prolazi");
-                    //salje da nije timeout
+                    setTimeout(function() {
+                        socket.write("GODINE_ME_STIZU");
+                    }, LAG);
                     break;
+
+                case "DUNI_VJETRE_MALO_PREKO_JETRE":            //symbolic meaning, not the same song as above
+                    setTimeout(function() {
+                        socket.write("UMRIJECU_OD_BOLA_IZGORJELO_SVE_OD_ALKOHOLA" + DELIMETER + messageBag + '\n');
+                    }, LAG);
+                    break;
+
                 default:
-                    console.log("Invalid BMK syntax");
+                    socket.write("KUPI_STRIKA_CIPELE_I_DADE_DZEPARAC");
                     break;
             }
 
@@ -40,5 +61,4 @@ net.createServer(function(socket) {
         .on("error", function(error) {
             console.log(error.toString());
         })
-
 }).listen(1337);
